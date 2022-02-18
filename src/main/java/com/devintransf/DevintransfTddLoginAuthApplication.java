b480295cd30d56e712da6ac112cfe5fcf9ed07d1
all-an <all-an@hotmail.com>
2022-02-18 17:20:33 -0300

    iniciando

---
+++ src/main/java/com/devintransf/DevintransfTddLoginAuthApplication.java
@@ -0,0 +1,13 @@
+package com.devintransf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevintransfTddLoginAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevintransfTddLoginAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devintransf/DevintransfTddLoginAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devintransf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevintransfTddLoginAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
