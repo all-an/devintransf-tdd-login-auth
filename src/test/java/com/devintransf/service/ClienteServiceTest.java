21b79c0665f7fb88360ee8b64722ad59c8acb39a
all-an <all-an@hotmail.com>
2022-02-24 10:16:07 -0300

    Criando teste ClienteService

--- src/test/java/com/devintransf/service/ClienteServiceTest.java
+++ src/test/java/com/devintransf/service/ClienteServiceTest.java
@@ -1,13 +1,21 @@
 package com.devintransf.service;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.devintransf.entity.Cliente;
 import com.devintransf.repository.ClienteRepository;
 
 @RunWith(SpringRunner.class)
@@ -16,10 +24,21 @@ import com.devintransf.repository.ClienteRepository;
 public class ClienteServiceTest {
 
 	@MockBean
-	ClienteRepository respository;
+	ClienteRepository repository;
+	
+	@Autowired
+	ClienteService service;
 	
 	@Before
 	public void setUp() {
-		BDDMockito.given(null)
+		BDDMockito.given(repository.findByEmailEquals(Mockito.anyString())).
+		willReturn(Optional.of(new Cliente()));
+	}
+	
+	@Test
+	public void testFindByEmail() {
+		Optional<Cliente> cliente = service.findByEmail("email@test.com");
+		
+		assertTrue(cliente.isPresent());
 	}
 }
