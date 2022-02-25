63ba1a368accc38973b1a8bf6a6b5b7f90a2b617
all-an <all-an@hotmail.com>
2022-02-25 08:39:41 -0300

    ClienteControllerTest criado

--- src/test/java/com/devintransf/controller/ClienteControllerTest.java
+++ src/test/java/com/devintransf/controller/ClienteControllerTest.java
@@ -1,16 +1,21 @@
 package com.devintransf.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.devintransf.entity.Cliente;
 import com.devintransf.service.ClienteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,6 +28,7 @@ public class ClienteControllerTest {
 	private static final String PASSWORD = "123456";
 	private static final String CPF = "09930093796";
 	
+	private static final String URL = "/user";
 	
 	@MockBean
 	ClienteService service;
@@ -30,8 +36,11 @@ public class ClienteControllerTest {
 	@Autowired
 	MockMvc mvc;
 	
-	public void testSave() {
-		
+	public void testSave() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayLoad())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated());
 	}
 
 	public Cliente getMockUser() {
@@ -46,5 +55,13 @@ public class ClienteControllerTest {
 	
 	public String getJsonPayLoad() {
 		ClienteDTO dto = new ClienteDTO();
+		
+		dto.setEmail(EMAIL);
+		dto.setCpf(CPF);
+		dto.setName(NAME);
+		dto.setPassword(PASSWORD);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(dto);
 	}
 }
