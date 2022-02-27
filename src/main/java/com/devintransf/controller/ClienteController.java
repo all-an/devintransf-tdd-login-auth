181e0656675946ae6c00abe93d388a52f135effb
all-an <all-an@hotmail.com>
2022-02-27 13:35:30 -0300

    controller create

--- src/main/java/com/devintransf/controller/ClienteController.java
+++ src/main/java/com/devintransf/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.devintransf.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devintransf.entity.Cliente;
 import com.devintransf.service.ClienteService;
 
 @RestController
@@ -22,6 +24,30 @@ public class ClienteController {
 	@PostMapping
 	public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto, BindingResult result){
 		Response<UserDTO> response = new Response<UserDTO>();
+		
+		Cliente cliente = service.save(this.convertDtoToEntity(dto));
+		
+		response.se
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	private Cliente convertDtoToEntity(ClienteDTO dto) {
+		Cliente c = new Cliente();
+		c.setEmail(dto.getEmail());
+		c.setName(dto.getName());
+		c.setPassword(dto.getPassword());
+		
+		return c;
+	}
+	
+	private ClienteDTO convertEntityToDto(Cliente c) {
+		ClienteDTO dto = new ClienteDTO();
+		dto.setEmail(c.getEmail());
+		dto.setName(c.getName());
+		dto.setPassword(c.getPassword());
+
+		return dto;
 	}
 	
 }
