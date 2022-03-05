d03f946fe4457fd068507c3a707c8227528072ed
all-an <all-an@hotmail.com>
2022-03-05 10:37:58 -0300

    Conta dto

--- src/main/java/com/devintransf/dto/ContaDTO.java
+++ src/main/java/com/devintransf/dto/ContaDTO.java
@@ -1,7 +1,21 @@
 package com.devintransf.dto;
 
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
 public class ContaDTO {
 
 	private Long id;
+	@Length(min=3)
+	@NotNull
+	private String name;
+	@NotNull
+	private BigDecimal value;
 	
 }
